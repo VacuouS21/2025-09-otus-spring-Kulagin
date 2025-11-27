5edc37a1db7545fef756229982ad46280f689368
VacuouS <vacuous21@gmail.com>
2025-11-27 21:32:01 +0300

    Правки

--- hw03/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw03/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -10,7 +10,6 @@ import ru.otus.hw.exceptions.QuestionReadException;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.List;
 
 @RequiredArgsConstructor
--- hw03/src/test/java/ru/otus/hw/dao/TestCsvQuestionDao.java
+++ hw03/src/test/java/ru/otus/hw/dao/TestCsvQuestionDao.java
@@ -11,7 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.when;
 
-class TestCSVQuestionDao {
+class TestCsvQuestionDao {
 
     @Mock
     private TestFileNameProvider fileNameProvider;
